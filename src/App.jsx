f38e9ffd2afe40fdecd57b8d93de53e79c3f0b78
LaurentD974 <laurentdevaud974@gmail.com>
2025-04-24 07:08:48 +0200

    Ok pour la page de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { usestate } from "react"
 
 function App() {
-const [nom,setNom]= usestate("coucou")
+
 
   return (
     <>
       <h1>Répertoire</h1>
       <ul>
-        <li>Nom</li>
+        <li>nom</li>
         <li>Prénom</li>
         <li>Téléphone</li>
       </ul>
