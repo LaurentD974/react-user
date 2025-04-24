251055f524ed1f876597cb44dd1e7387a8df3b81
LaurentD974 <laurentdevaud974@gmail.com>
2025-04-24 07:03:13 +0200

    J'ai rien qui s'affiche ??

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { usestate } from "react"
 
 function App() {
+const [nom,setNom]= usestate("coucou")
 
   return (
     <>
       <h1>Répertoire</h1>
-      
+      <ul>
+        <li>Nom</li>
+        <li>Prénom</li>
+        <li>Téléphone</li>
+      </ul>
+      <button>Mettre à jour</button>
+      <button>Favoris</button>
     </>
   )
 }
